008d5a46e1784f7bfb33d2073bd45012e1cd1331
Moses Arthur <mosesarthur@outlook.dk>
2019-07-11 11:20:37 +0200

    Update app.py
    general version of getting the browser

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+#windows version
 import speech_recognition as sr
 import webbrowser as wb
 
@@ -15,6 +16,33 @@ try:
 
     f_text = 'https://www.google.com/search?q=' + text
     wb.get(path).open(f_text)
+
+except Exception as e:
+    print(e)
+    
+    
+ #gerneral version
+import speech_recognition as sr
+import webbrowser as wb
+import os
+
+
+r = sr.Recognizer()
+
+with sr.Microphone() as source:
+    r.adjust_for_ambient_noise(source)
+    os.system("Say Say something")
+    print('Say Something!')
+    audio = r.listen(source)
+    print('Done!')
+
+try:
+    text = r.recognize_google(audio, language="EN")
+    print('Google thinks you said:\n' + text)
+    wb.get("chrome")
+    wb.open_new('https://www.google.com/search?q=' + text)
+
+
 
 except Exception as e:
     print(e)
